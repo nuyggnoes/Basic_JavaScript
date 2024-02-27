813a8fc9843947594b17171299d78b11aeded55f
임성균 <97024187+nuyggnoes@users.noreply.github.com>
2024-02-27 17:19:22 +0900

    .

--- 0226/login.js
+++ 0226/login.js
@@ -2,6 +2,7 @@ const loginForm = document.querySelector(".login-form");
 const loginDiv = document.querySelector(".login");
 const loginInput = loginForm.querySelector(".login-input");
 const userBtn = document.querySelector(".nav-item-downloadBtn a");
+const todoContainer = document.querySelector(".todo-container");
 const greeting = document.querySelector(".greeting");
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
@@ -15,6 +16,7 @@ function onLoginSubmit(event) {
   const username = loginInput.value;
   //   localStorage.setItem(USERNAME_KEY, username);
   loginForm.classList.add(HIDDEN_CLASSNAME);
+  todoContainer.classList.remove(HIDDEN_CLASSNAME);
   userBtn.innerText = `Hello! ${username}`;
   paintGreeting(username);
 }
---
+++ 0226/todo.js
@@ -0,0 +1,4 @@
+const todoInput = document.querySelector(".todo-form input");
+const todoBtn = document.querySelector(".todo-form button");
+
+function addTodo() {}
